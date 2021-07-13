df54c7ece80521e9d2fcaaae4aa78c2ff21f1ac6
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-07-13 20:20:01 +0300

    sprendiniai 2+3 pilnai

--- js/main2.js
+++ js/main2.js
@@ -1,12 +1,14 @@
 const allIngredientsDOM = document.querySelectorAll('li'); //pereina per sąrašą
-let a = [];
+const ingredientai = [];
 
 for (let i = 0; i < allIngredientsDOM.length; i++) {
     const item = allIngredientsDOM[i];
     const itemName = item.innerText; // su innerText išgaunam reikšmę
-    console.log(itemName);
+    
     // keitimas žodiu i didziasias raides
     const nameInUpperCase = itemName.toUpperCase();
-    a.push(nameInUpperCase);
+    console.log(itemName);
+
+    ingredientai.push(nameInUpperCase);
 }
-console.log(a);
+console.log(ingredientai);
--- js/main3.js
+++ js/main3.js
@@ -2,11 +2,12 @@ const h1DOM = document.querySelector('h1')
 
 // klases simba pridejimas. Pirmiausia kreipiamasi i elementa h2DOM, tada reikia prideti klases
 
-h2DOM.classList.add('simba');
+h1DOM.classList.add('simba');
 
-const allIngredientsDOM = document.querySelectorAll('.visi');
-
-for (let = 0; i < allIngredientsDOM.length; i++) {
-    const item = allIngredientsDOM[i];
-    item.classList.add ('darzove')
+const allLiDOM = document.querySelectorAll('li');
+// eik per kiekvieną elementą ir padaryk analogišką komandą: h1DOM.classList.add, t.y. kreipkis i kiekviena
+//to ind.list'o klasiu sarasa ir pridek klase
+for (let i = 0; i < allIngredientsDOM.length; i++) {
+    const itemDOM = allLiDOM[i]; //itasis visu DOM elementu narys
+    itemDOM.classList.add ('darzove') // pridedam klase 'darzove'
 }
