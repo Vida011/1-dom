5557c904a7f0df24f6d76af3f73865b03c738bd5
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-07-13 20:20:38 +0300

    sprendiniai 2+3 pilnai pap.

--- js/main3.js
+++ js/main3.js
@@ -9,5 +9,5 @@ const allLiDOM = document.querySelectorAll('li');
 //to ind.list'o klasiu sarasa ir pridek klase
 for (let i = 0; i < allIngredientsDOM.length; i++) {
     const itemDOM = allLiDOM[i]; //itasis visu DOM elementu narys
-    itemDOM.classList.add ('darzove') // pridedam klase 'darzove'
+    itemDOM.classList.add ('darzove'); // pridedam klase 'darzove'
 }
